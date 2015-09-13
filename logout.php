3f81ba99742b5597a34e798b8965526ed783db78
Yu-Tzu Lin <linyutsu23@gmail.com>
2015-09-13 12:04:59 +0800

    logout.php complete

--- logout.php
+++ logout.php
@@ -2,13 +2,13 @@
 	session_start(); 
 	require_once(dirname(__FILE__) . "/config.php"); 
 	require_once(dirname(__FILE__) . "/lib/std.php");
-	if(isset($_SESSION['loginID'] && isset($_SESSION['loginToken']){
+	if(isset($_SESSION['loginID']) && isset($_SESSION['loginToken'])){
 		if(checkExist($DBmain, $_SESSION['loginID'], $_SESSION['loginToken'])){
 			setLog($DBmain, "info", "Log out", $_SESSION['stuID']); 
 			session_destroy(); 
 		}
 		else
-			setLog($DBmain, "warning", "login ID or Token Error. ", $_SESSION['loginID']; 
+			setLog($DBmain, "warning", "login ID or Token Error. ", $_SESSION['loginID']); 
 	}
 	locate($URLPv . "index.php"); 
 ?>
