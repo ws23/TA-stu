eeab7588e722e7fb31da34cb052cb8f53386e631
Yu-Tzu Lin <linyutsu23@gmail.com>
2015-09-13 12:59:43 +0800

    add menu page

---
+++ menu.php
@@ -0,0 +1,4 @@
+<p class="text-center">
+	<a href="#"><button class="btn btn-danger">教學助理申請（未開放）</button></a>
+	<a href="<?php echo $URLPv; ?>index.php?module=list"><button class="btn btn-info">填寫工作月誌</button></a>
+</p>
