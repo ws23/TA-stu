678eefd677a40a8c148418660f917a39bdf733ea
Yu-Tzu Lin <linyutsu23@gmail.com>
2015-10-15 16:16:45 +0800

    update title

--- lib/header.php
+++ lib/header.php
@@ -8,12 +8,12 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="index.php">國立東華大學 通識教育中心 TA系統</a>
+                        <a class="navbar-brand" href="index.php">國立東華大學通識教育中心教學助理(TA)工作月誌登記系統</a>
                     </div>
                     <div id="navbar" class="navbar-collapse collapse">
                         <ul class="nav navbar-nav navbar-right">
 							<?php if(isset($_SESSION['loginID'])){ 	?>
-							<li><a href="<?php echo $URLPv; ?>logout.php">登出</a></li>
+							<li><a href="<?php echo $URLPv; ?>logout.php">Logout</a></li>
 							<?php } ?>
                         </ul>
                     </div><!--/.nav-collapse -->
